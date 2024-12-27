d8273b228b2e24e79b45c0ebc4ec005212a3b183
rezaderakhshan <rezaderakhshanich@gmail.com>
2024-12-27 20:00:49 +0330

    handling remove filter date

--- src/components/SortingDate.tsx
+++ src/components/SortingDate.tsx
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
+import { Box, IconButton } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 import { DateObject } from "react-multi-date-picker";
 import DateSelection from "./DateSelection";
 import { useAppDispatch } from "../hooks/hooks";
 import { setIsoStateFilterDate } from "../features/todo/todoSlice";
-import { Box } from "@mui/material";
 
 const SortingDate = () => {
   const dispatch = useAppDispatch();
   const [sortingDate, setSortingDate] = useState<DateObject | null>(null);
   const [isoSortingDate, setIsoSortingDate] = useState("");
-
+  const resetFilterDate = () => {
+    setIsoSortingDate("");
+    setSortingDate(null);
+  };
   useEffect(() => {
     dispatch(setIsoStateFilterDate(isoSortingDate));
   }, [isoSortingDate, dispatch]);
   return (
-    <Box>
+    <Box
+      sx={{
+        position: "relative",
+      }}
+    >
       <DateSelection
         label="فیلتر براساس تاریخ"
         value={sortingDate}
         setValue={setSortingDate}
         setIsoState={setIsoSortingDate}
       />
+      {isoSortingDate.length > 0 && (
+        <IconButton
+          sx={{ position: "absolute", bottom: -40, left: 0 }}
+          onClick={resetFilterDate}
+        >
+          <ClearIcon color="primary" />
+        </IconButton>
+      )}
     </Box>
   );
 };
